c79a59f8a6cf463d34da3bb31a6a0482d9bc9fae
MichalKlemm <Klemmmichal@lgmail.com>
2024-06-05 20:26:51 +0200

    Vytvoření Uuid generátoru.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+
+        System.out.println("Your UUID is: " + uuidAsString);
+    }
+}
